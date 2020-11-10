1411ace7bd51e5168523dd816975ffe978ea7a1b
BoilIngColDx <73469926+BoilIngColDx@users.noreply.github.com>
2020-11-10 15:26:01 +0000

    Initial commit

---
+++ Game1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JumpThing
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D backgroundTxr, playerSheetTxr, platformSheetTxr, whiteBox;
+
+        Point screenSize = new Point(800, 450);
+
+        PlayerSprite playerSprite;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = screenSize.X;
+            _graphics.PreferredBackBufferHeight = screenSize.Y;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            backgroundTxr = Content.Load<Texture2D>("JumpThing_background");
+            playerSheetTxr = Content.Load<Texture2D>("JumpThing_spriteSheet1");
+            platformSheetTxr = Content.Load<Texture2D>("JumpThing_spriteSheet2");
+            
+            whiteBox = new Texture2D(GraphicsDevice, 1, 1);
+            whiteBox.SetData(new[] { Color.White });
+
+            playerSprite = new PlayerSprite(playerSheetTxr, whiteBox, new Vector2(0, 0));
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(backgroundTxr, new Rectangle(0, 0, screenSize.X, screenSize.Y), Color.White);
+
+            playerSprite.Draw(_spriteBatch, gameTime);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Sprite.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JumpThing
+{
+    class Sprite
+    {
+        public Texture2D spriteSheestTxr, collisionTxr;
+        public Vector2 spriteLocation, spriteVelocity, spriteOffset, collisionScale, collisionOffset;
+        public bool isDead, isColliding, drawCollision, isFlipped;
+
+        public int currentAnim, currentFrame;
+        public float frameTime, frameCounter;
+        public List<List<Rectangle>> animations;
+
+        public Sprite(Texture2D newSpriteSheet, Texture2D newCollisionTxt, Vector2 newLocation)
+        {
+            spriteSheestTxr = newSpriteSheet;
+            collisionTxr = newCollisionTxt;
+            spriteLocation = newLocation;
+
+            spriteOffset = new Vector2(0.5f, 0.5f);
+            spriteVelocity = new Vector2(0f, 0f);
+            collisionScale = new Vector2(1f, 1f);
+            collisionOffset = new Vector2(0f, 0f);
+            isColliding = false;
+            drawCollision = false;
+            currentAnim = 0;
+            currentFrame = 0;
+            frameTime = 0.5f;
+            frameCounter = frameTime;
+
+            animations = new List<List<Rectangle>>();
+
+            animations.Add(new List<Rectangle>());
+            animations[0].Add(new Rectangle(0, 0, 48, 48));
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            if (animations[currentFrame].Count > 1)
+            {
+                frameCounter -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (frameCounter <= 0)
+                {
+                    frameCounter = frameTime;
+                    currentFrame++;
+                    if (currentFrame >= animations[currentAnim].Count) currentFrame = 0;
+                }
+            }
+            spriteBatch.Draw(
+                spriteSheestTxr,
+                new Rectangle(0, 0, 48, 48),
+                animations[currentAnim][currentFrame],
+                Color.White,
+                0f,
+                new Vector2(0, 0),
+                SpriteEffects.None,
+                1f
+                );
+        }
+
+
+    }
+}
